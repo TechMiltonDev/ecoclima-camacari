7fa06ab88462e257ec37be2ec967b3706f111446
Edmilton Barbosa <techmiltondev@gmail.com>
2026-09-06 21:59:12 -0300

    mudando de fonte de clima

--- src/services/coletarClima.js
+++ src/services/coletarClima.js
@@ -1,12 +1,16 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { PrevisaoHistorica } = require("../models/index")
+const { PrevisaoHistorica } = require('../models/index');
 const { getAttrSafe, getTextSafe, getDateBr } = require('../utils/helpers');
 
-async function coletarClima(salvarDados = false, cidade = 'Camaçari-BA') {
+async function coletarClimaAtual(
+  salvarDados = false,
+  cidade = { nome: 'Camacari-BA', id: 892 },
+) {
   try {
-    const cidadeFormatada = cidade
-    .replaceAll(' ', '-')
+    const idCidade = cidade.id;
+    const cidadeFormatada = cidade.nome
+      .replaceAll(' ', '-')
       .normalize('NFD')
       .replace(/[Çç]/g, 'c')
       .replace(/[\u0300-\u036f]/g, '')
@@ -15,144 +19,82 @@ async function coletarClima(salvarDados = false, cidade = 'Camaçari-BA') {
       .toLowerCase();
 
     const response = await axios.get(
-      `https://www.otempo.com.br/tempo/${cidadeFormatada}`,
+      `https://www.climatempo.com.br/previsao-do-tempo/agora/cidade/${idCidade}/${cidadeFormatada}`,
     );
     const html = response.data;
     const $ = cheerio.load(html);
 
-    const climaAtual = $(
-      'div.weather-card__middle div.weather-card__current-weather',
-    )
+    const climaAtual = $('.now-forecast-card')
       .map((i, el) => {
         const $el = $(el);
 
         // Agora use $el.find em vez de el.find
-        let $imgClima = $el.find('div img.weather-card__current-weather__icon');
+        let $imgClima = $el.find('img.now-forecast-card__weather-icon');
 
-        let $elDiv = $el.find('div.d-flex.flex-column');
+        let $elTemp = $el.find('span.now-forecast-card__temperature');
 
-        let $elTemp = $elDiv.find(
-          'span.weather-card__current-weather__temperature',
-        );
+        const metricas = {
+          vento: '',
+          umidade: '',
+          sensacaoTermica: '',
+        };
 
-        let $elClima = $elDiv.find(
-          'span.weather-card__current-weather__condition-name',
-        );
-            // DEBUG: Verificar se getDateBr ainda é função aqui
+        $el.find('.now-forecast-card__metric').each((i, el) => {
+          const nome = $(el)
+            .find('.now-forecast-card__metric-heading')
+            .text()
+            .replace(/\s+/g, '');
+          let valor = $(el)
+            .find('.now-forecast-card__metric-value')
+            .text()
+            .replace(/\s+/g, '');
+
+          valor = valor.includes('-') ? valor.split('-')[1] : valor;
+          //const linkIcon = $(el)
+          //   .find(
+          //     '.now-forecast-card__metric-icon.now-forecast-card__metric-icon--wind',
+          //   )
+          //   .attr('src')
+          //   .replace(/\s+/g, ''),
+          if (nome.toLowerCase().includes('vento')) {
+            metricas.vento = valor;
+          } else if (nome.toLowerCase().includes('umidade')) {
+            metricas.umidade = valor;
+          } else if (
+            nome.toLowerCase().includes('sensacao') ||
+            nome.includes('sensaçãotérmica') ||
+            nome.includes('sensa')
+          ) {
+            metricas.sensacaoTermica = valor;
+          }
+        });
+
         return {
           horario: getDateBr().toLocaleTimeString('en-US', {
-    		hour: '2-digit',
-    		minute: '2-digit',
-   		 second: '2-digit',
- 		   hour12: false // Garante formato 24h (ex: 21:00 em vez de 09:00 PM)
- 		 }),
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false, // Garante formato 24h (ex: 21:00 em vez de 09:00 PM)
+          }),
           temperatura: getTextSafe($elTemp)
             .replace(/\s+/g, '')
             .replaceAll('°', ''),
-          clima: getTextSafe($elClima),
-          linkClima: getAttrSafe($imgClima, 'src').replace(/\s+/g, ''),
-        };
-      })
-      .get();
-
-    const previsaoDia = $(
-      'div.weather-card__forecast div.weather-card__forecast__box',
-    )
-      .map((i, el) => {
-        return {
-          horario: $(el)
-            .find('div.weather-card__forecast__box__number')
-            .text()
-            .replace(/\s+/g, ''),
-          temperatura: $(el)
-            .find('div.weather-card__forecast__box__temperature')
-            .text()
-            .replace(/\s+/g, '')
-            .replaceAll('°', ''),
-          clima: $(el)
-            .find('div.weather-card__forecast__box__icon img')
-            .attr('alt')
-            .trim(),
-          linkClima: $(el)
-            .find('div.weather-card__forecast__box__icon img')
-            .attr('src')
-            .replace(/\s+/g, ''),
+          vento: metricas.vento,
+          umidade: metricas.umidade,
+          sensacaoTermica: metricas.sensacaoTermica,
+          clima: getAttrSafe($imgClima, 'alt'),
+          linkClima:
+            'https://www.climatempo.com.br' +
+            getAttrSafe($imgClima, 'src').replace(/\s+/g, ''),
         };
       })
       .get();
 
     const horarioAtual = new Date(getDateBr()).getHours();
-    console.log(horarioAtual)
-    if (horarioAtual <= parseInt(previsaoDia[0].horario)) {
-      previsaoDia.shift();
-    }
-
-    const infoDia = $(
-      'div.weather-card__info-panel div.weather-card__info-panel__box',
-    )
-      .map((i, el) => {
-        return {
-          nome: $(el)
-            .find('div.weather-card__info-panel__box__name')
-            .text()
-            .replace(/\s+/g, ''),
-          valor: $(el)
-            .find('div.weather-card__info-panel__box__description')
-            .text()
-            .replace(/\s+/g, ''),
-          linkIcon: $(el)
-            .find('div.weather-card__info-panel__box__icon img')
-            .attr('src')
-            .replace(/\s+/g, ''),
-        };
-      })
-      .get();
-
-    const previsoesDias = $(
-      'div.aem-GridColumn.aem-GridColumn--default--4.aem-GridColumn--phone--12.mt-4.mt-md-0 ul.weather-list-forecast li.weather-list-forecast__item div.weather-list-forecast__item__box-min',
-    )
-      .map((i, el) => {
-        const $el = $(el);
-        // const $item = $el.parent().parent(); pegar o avô do elemento
-        const $item = $el.closest('li'); // sobe até achar o primeiro match
-
-        // Agora use $el.find em vez de el.find
-        let $imgClima = $item.find(
-          'div.weather-list-forecast__item__right img.weather-list-forecast__item__icon',
-        );
-
-        if ($imgClima.length === 0) {
-          $imgClima = $item.find('div.weather-list-forecast__item__right img');
-        }
-        return {
-          nomeDia: $item
-            .find('h3.weather-list-forecast__item__title')
-            .text()
-            .trim(),
-          dia: $item
-            .find('p.weather-list-forecast__item__description')
-            .text()
-            .trim(),
-          tempMin: $item
-            .find(
-              'div.weather-list-forecast__item__middle span.weather-list-forecast__item__box-min__description',
-            )
-            .text()
-            .replace(/\s+/g, '')
-            .replaceAll('°', ''),
-          tempMax: $item
-            .find(
-              'div.weather-list-forecast__item__middle span.weather-list-forecast__item__box-max__description',
-            )
-            .text()
-            .replace(/\s+/g, '')
-            .replaceAll('°', ''),
-          // Usa a função segura: se não tiver imagem, retorna string vazia em vez de crashar
-          clima: getAttrSafe($imgClima, 'alt'),
-          linkClima: getAttrSafe($imgClima, 'src').replace(/\s+/g, ''),
-        };
-      })
-      .get();
+    console.log(horarioAtual);
+    // if (horarioAtual <= parseInt(previsaoDia[0].horario)) {
+    //   previsaoDia.shift();
+    // }
 
     // INTEGRACAO COM MODELOS ---
     if (salvarDados) {
@@ -160,10 +102,6 @@ async function coletarClima(salvarDados = false, cidade = 'Camaçari-BA') {
         // Combinar climaAtual com infoDia para a previsão diária
         const dadosDiaria = {
           ...climaAtual[0],
-          infoDia: infoDia.map((item) => {
-            return { nome: item.nome, valor: item.valor };
-          }), // Passa o array completo de infoDia
-          cidade: cidade,
         };
 
         // Adicionar registro à Tabela 3 (Histórico) - COPIA os dados da Tabela 1, incluindo infoDia
@@ -175,9 +113,7 @@ async function coletarClima(salvarDados = false, cidade = 'Camaçari-BA') {
 
     return {
       dadosAtual: climaAtual[0],
-      infoDia,
       cidade,
-      dadosSemanais: previsoesDias,
     };
   } catch (error) {
     console.error('Erro ao coletar dados do clima:', error.message);
@@ -190,4 +126,112 @@ async function coletarClima(salvarDados = false, cidade = 'Camaçari-BA') {
   }
 }
 
-module.exports = coletarClima;
+async function coletarClimaHoje(cidade = 'Camaçari-BA') {
+  const idCidade = cidade.id;
+  const cidadeFormatada = cidade.nome
+    .replaceAll(' ', '-')
+    .normalize('NFD')
+    .replace(/[Çç]/g, 'c')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-zA-Z0-9-]/g, '') // ← Adicionado o hífen aqui
+    .replace(/\s+/g, '')
+    .toLowerCase();
+
+  const response = await axios.get(
+    `https://www.climatempo.com.br/previsao-do-tempo/cidade/${idCidade}/${cidadeFormatada}`,
+  );
+  const html = response.data;
+  const $ = cheerio.load(html);
+  const previsaoDia = $('div.hourly-forecast-carousel__track')
+    .map((i, el) => {
+      return {
+        horario: $(el)
+          .find('.hourly-forecast-card__time')
+          .text()
+          .replace(/\s+/g, ''),
+        temperatura: $(el)
+          .find('.hourly-forecast-card__temp')
+          .text()
+          .replace(/\s+/g, '')
+          .replaceAll('°', ''),
+        clima: $(el)
+          .find('.hourly-forecast-card__weather-icon')
+          .attr('alt')
+          .trim(),
+        linkClima:
+          'https://www.climatempo.com.br/' +
+          $(el)
+            .find('.hourly-forecast-card__weather-icon')
+            .attr('data-src')
+            .replace(/\s+/g, ''),
+      };
+    })
+    .get();
+
+  const infoDia = $('div.today-forecast-card__grid')
+    .map((i, el) => {
+      return {
+        nome: $(el)
+          .find('.daily-variables-grid__title')
+          .text()
+          .replace(/\s+/g, ''),
+        valor: $(el).find('.daily-variables-grid__values').text().trim(),
+        linkIcon:
+          'https://www.climatempo.com.br/' +
+          $(el)
+            .find('.daily-variables-grid__icon')
+            .attr('src')
+            .replace(/\s+/g, ''),
+      };
+    })
+    .get();
+}
+
+async function coletarClimaSemana(cidade = 'Camaçari-BA') {
+  const idCidade = cidade.id;
+  const cidadeFormatada = cidade.nome
+    .replaceAll(' ', '-')
+    .normalize('NFD')
+    .replace(/[Çç]/g, 'c')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-zA-Z0-9-]/g, '') // ← Adicionado o hífen aqui
+    .replace(/\s+/g, '')
+    .toLowerCase();
+
+  const response = await axios.get(
+    `https://www.climatempo.com.br/previsao-do-tempo/15-dias/cidade/${idCidade}/${cidadeFormatada}`,
+  );
+  const html = response.data;
+  const $ = cheerio.load(html);
+  const previsoesDias = $('.agg-daily__header')
+    .map((i, el) => {
+      const $el = $(el);
+      // const $item = $el.parent().parent(); pegar o avô do elemento
+
+      // Agora use $el.find em vez de el.find
+      let $imgClima = $item.find('.agg-daily__icon.lazyloaded');
+
+      return {
+        nomeDia: $item.find('.weekday').text().trim(),
+        dia: $item.find('.date-inside-circle__day').text().trim(),
+        tempMin: $item
+          .find('.agg-daily__temp.-min')
+          .text()
+          .replace(/\s+/g, '')
+          .replaceAll('°', ''),
+        tempMax: $item
+          .find('.agg-daily__temp.-max')
+          .text()
+          .replace(/\s+/g, '')
+          .replaceAll('°', ''),
+        // Usa a função segura: se não tiver imagem, retorna string vazia em vez de crashar
+        clima: getAttrSafe($imgClima, 'alt'),
+        linkClima:
+          'https://www.climatempo.com.br/' +
+          getAttrSafe($imgClima, 'data-src').replace(/\s+/g, ''),
+      };
+    })
+    .get();
+}
+
+module.exports = { coletarClimaAtual, coletarClimaHoje, coletarClimaSemana };
